e3d2a196411c563670d75c14b5dadc9bd32ee02e
Mostafizur100 <136628235+Mostafizur100@users.noreply.github.com>
2023-10-14 23:23:38 +0600

    Update Find_String.py
    In this challenge, the user enters a string and a substring. You have to print the number of times that the substring occurs in the given string. String traversal will take place from left to right, not from right to left.
    
    NOTE: String letters are case-sensitive.

--- Find_String.py
+++ Find_String.py
@@ -1,1 +1,20 @@
+string=input()
+matching=input()
+m=0
+n=len(matching)
+sum1=0
+final=0
 
+for i in range(len(string)-len(matching)+1):
+    # print(i)
+    # print(f"m1={m}")
+    # print(f"n1={n}")
+    if string[m:n]==matching:
+        # print(string[m:n])
+        final=sum1+1
+        sum1=final
+    m=m+1
+    # print(f"m={m}")
+    n=n+1
+    # print(f"n={n}")
+print(final)
